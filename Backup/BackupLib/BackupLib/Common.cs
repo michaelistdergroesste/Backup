e79b50063e0d27781425687cce754f130b7db503
Michael Kerres <m.kerres@gmx.net>
2022-02-05 08:29:57 +0100

    erste lib - Datei

---
+++ Backup/BackupLib/BackupLib/Common.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackupLib
+{
+    internal class Common
+    {
+        /// <summary>
+        /// Die maximale Anzahl der Pfade, die gesichert wird. Dies entspricht der Anzahl der Pfade auf dem Main Screen
+        /// </summary>
+        protected int pathNumber;
+    }
+}
